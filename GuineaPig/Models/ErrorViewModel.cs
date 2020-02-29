f2096035ef6dd2d4c000bded1ffd38559d833897
Eric Papas <epaps33@gmail.com>
2020-02-28 23:11:34 -0500

    Add project files.

---
+++ GuineaPig/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace GuineaPig.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
